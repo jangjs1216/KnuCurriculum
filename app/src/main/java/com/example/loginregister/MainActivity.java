1f37e943c8d5d9b0455b79e8e781c5c035d0d0e0
박경무 <ruddd8256@naver.com>
2021-08-03 01:26:27 +0900

    과목코드 추가

--- app/src/main/java/com/example/loginregister/MainActivity.java
+++ app/src/main/java/com/example/loginregister/MainActivity.java
@@ -4,14 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
+
+    //과목코드 해시함수로 배열화 과목코드넣으면 과목명이랑 학점나옴
+    HashMap<String, Object> subjectCode =  new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +44,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        //과목코드 받아오기 함수시작
+        try {
+            readFromAssets("subjectCode.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    //과목코드 받아오기 함수
+    private void  readFromAssets(String filename) throws Exception {
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(filename)));
+
+        String line = reader.readLine();
+        int counti=0;
+
+        while(line != null) {
+
+            String[] Sarray = line.split("\t");
+            String Scode = Sarray[0];
+            String[] Sname = {Sarray[1],Sarray[2]};
+            subjectCode.put(Sarray[0],Sname);
+
+            ++counti;
+            line = reader.readLine();
+        }
+        reader.close();
     }
 }
