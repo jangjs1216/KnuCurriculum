afaf70efc84a74e7f12acc32a38160c1534786bd
jangjs1216 <jangjs1216@naver.com>
2021-08-08 23:53:35 +0900

    8/8 안승재 Fragment4 액션바 구성

--- app/src/main/java/com/example/loginregister/Fragment4.java
+++ app/src/main/java/com/example/loginregister/Fragment4.java
@@ -2,17 +2,55 @@ package com.example.loginregister;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Fragment4 extends Fragment {
+    private Toolbar toolbar;
+    private final static  String TAG = "Frag4";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_4, container, false);
+        View view =inflater.inflate(R.layout.fragment_4, container, false);
+        toolbar = (Toolbar)view.findViewById(R.id.tb_frag4);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ActionBar actionBar = ((MainActivity)getActivity()).getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);//커스텀액션바사용
+        actionBar.setDisplayShowTitleEnabled(false);//기본제목을 없애줍니다.
+        setHasOptionsMenu(true);
+        return view;
     }
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.actionbar_frag1,menu);
+        Log.e(TAG,"sex");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_btn_setting:
+                //설정프래그먼트 오픈
+                break;
+            case android.R.id.home:
+                //뒷프래그먼트로 이동;
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
