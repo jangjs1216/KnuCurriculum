a0d6697091f9e9e2d70c89b44ab30317d7addd04
jangjs1216 <jangjs1216@naver.com>
2021-08-08 23:53:22 +0900

    8/8 안승재 Fragment3 액션바 구성

--- app/src/main/java/com/example/loginregister/Fragment3.java
+++ app/src/main/java/com/example/loginregister/Fragment3.java
@@ -2,17 +2,53 @@ package com.example.loginregister;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Fragment3 extends Fragment {
+    private View view;
+    private Toolbar toolbar;
+    private final static String TAG ="Frag3";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        view = inflater.inflate(R.layout.fragment_3, container, false);
+        toolbar = (androidx.appcompat.widget.Toolbar)view.findViewById(R.id.tb_frag3);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ActionBar actionBar = ((MainActivity)getActivity()).getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);//커스텀액션바사용
+        actionBar.setDisplayShowTitleEnabled(false);//기본제목을 없애줍니다.
+        setHasOptionsMenu(true);
+        return view;
+    }
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.actionbar_frag3,menu);
+        Log.e(TAG,"sex");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_btn_search:
+                //검색프래그먼트 오픈
+                break;
+            case R.id.action_btn_create:
+                //글쓰기 프래그먼트로 이동;
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
