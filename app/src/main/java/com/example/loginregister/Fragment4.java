9ea038b5a366d88d72660f90ea8fb8f90b2d62cd
sonbe <sonbe8704@gmail.com>
2021-08-09 15:24:16 +0900

    8월 9일 안승재 Frag4 설정창 & 뒤로가기버튼

--- app/src/main/java/com/example/loginregister/Fragment4.java
+++ app/src/main/java/com/example/loginregister/Fragment4.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,12 +21,16 @@ import org.jetbrains.annotations.NotNull;
 
 public class Fragment4 extends Fragment {
     private Toolbar toolbar;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
     private final static  String TAG = "Frag4";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_4, container, false);
+        fm = getActivity().getSupportFragmentManager();
+        ft= fm.beginTransaction();
         toolbar = (Toolbar)view.findViewById(R.id.tb_frag4);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ActionBar actionBar = ((MainActivity)getActivity()).getSupportActionBar();
@@ -44,7 +50,9 @@ public class Fragment4 extends Fragment {
     public boolean onOptionsItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_btn_setting:
-                //설정프래그먼트 오픈
+                ft.replace(R.id.main_frame, new Setting_Container_Fragment());
+                ft.addToBackStack(null);
+                ft.commit();
                 break;
             case android.R.id.home:
                 //뒷프래그먼트로 이동;
