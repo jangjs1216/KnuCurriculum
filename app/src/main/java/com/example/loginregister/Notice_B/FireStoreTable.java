2c95c8054a9f0c38eec62b9a158c71df2db08581
최정인 <rmfotjanj1234@naver.com>
2021-08-10 23:02:48 +0900

    [20210810 최정인] FireStore에 올릴 테이블 객체

---
+++ app/src/main/java/com/example/loginregister/Notice_B/FireStoreTable.java
@@ -0,0 +1,22 @@
+package com.example.loginregister.Notice_B;
+
+import java.util.Map;
+
+public class FireStoreTable {
+    Map<String, Map<String, String>> table;
+
+    public FireStoreTable() {
+    }
+
+    public FireStoreTable(Map<String, Map<String, String>> table) {
+        this.table = table;
+    }
+
+    public Map<String, Map<String, String>> getTable() {
+        return table;
+    }
+
+    public void setTable(Map<String, Map<String, String>> table) {
+        this.table = table;
+    }
+}
