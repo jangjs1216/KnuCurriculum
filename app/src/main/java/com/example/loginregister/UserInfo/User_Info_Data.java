b0f5e2d6954720aa684144217a26cab1dfbdec79
sonbe <sonbe8704@gmail.com>
2021-09-08 01:25:28 +0900

    [20210908 안승재] 유저스펙관리

--- app/src/main/java/com/example/loginregister/UserInfo/User_Info_Data.java
+++ app/src/main/java/com/example/loginregister/UserInfo/User_Info_Data.java
@@ -1,8 +1,15 @@
 package com.example.loginregister.UserInfo;
 
 public class User_Info_Data {
+    private String type;
     private String user_info_title;
     private String user_info_content;
+
+    public User_Info_Data(String type, String user_info_title, String user_info_content) {
+        this.type = type;
+        this.user_info_title = user_info_title;
+        this.user_info_content = user_info_content;
+    }
 
     public String getUser_info_title() {
         return user_info_title;
@@ -21,9 +28,13 @@ public class User_Info_Data {
     }
 
     public User_Info_Data(){}
-    public User_Info_Data(String user_info_title, String user_info_content) {
-        this.user_info_title = user_info_title;
-        this.user_info_content = user_info_content;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
 
